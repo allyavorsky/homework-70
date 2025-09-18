6fd1816c2a0043a0186edd4fe06d911814ba2cc1
allyavorsky <allyavorsky@proton.me>
2025-09-18 14:38:13 +0300

    HW70-1: user schema with validation and indexing

--- models/User.js
+++ models/User.js
@@ -3,12 +3,15 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "Поле email є обов'язковим"],
     unique: true,
+    match: [/.+\@.+\..+/, "Будь ласка, введіть коректний email"],
+    index: true,
   },
   password: {
     type: String,
-    required: true,
+    required: [true, "Поле пароль є обов'язковим"],
+    minlength: [6, "Пароль повинен містити щонайменше 6 символів"],
   },
 });
 
